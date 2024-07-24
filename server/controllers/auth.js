aec5dd67a989fe2ca5444803ce5925c854d033a8
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-24 11:33:40 +0530

    Auth Controller Completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,43 +3,43 @@ import User from "../models/User.js"
 import jwt from "jsonwebtoken"
 
 export const register = async (req, res, next) => {
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
   const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({message:"Email already exists"});
+  if (userExists) return res.status(400).json({ message: "Email already exists" });
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
-  try{
+  try {
     const newUser = new User({
       name: name,
       email: email,
       password: hash
     })
-    await newUser.save();    
+    await newUser.save();
     const accessToken = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
 
     const options = {
       httpOnly: true,
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-      sameSite: 'strict' 
+      sameSite: 'strict'
     };
 
     const { password, isAdmin, ...otherDetails } = newUser._doc;
     return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .json({ user: { ...otherDetails }, isAdmin });
-  }catch(err){
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json({ user: { ...otherDetails }, isAdmin });
+  } catch (err) {
     next(err);
   }
 }
 
-export const login = async(req, res, next)=>{
-  try{
-    const user = await User.findOne({email: req.body.email})
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
 
     if (!user || !await bcrypt.compare(req.body.password, user.password)) {
-      return res.status(400).json({message:"Wrong email or password"});
+      return res.status(400).json({ message: "Wrong email or password" });
     }
     const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
     console.log(accessToken)
@@ -47,21 +47,21 @@ export const login = async(req, res, next)=>{
       httpOnly: true,
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-      sameSite: 'strict' 
+      sameSite: 'strict'
     };
 
     const { password, isAdmin, ...otherDetails } = user._doc;
     return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .json({ user: { ...otherDetails }, isAdmin });
-  }catch(err){
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json({ user: { ...otherDetails }, isAdmin });
+  } catch (err) {
     next(err)
   }
 }
 
 export const logout = async (req, res, next) => {
-  try{
+  try {
     res.clearCookie("accessToken", {
       httpOnly: true,
       secure: true,
