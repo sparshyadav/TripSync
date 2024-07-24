6a2800d641d58119b5fa95e51e23f4f0cd6b223f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-24 11:34:00 +0530

    Payments Controllers Completed

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -4,7 +4,7 @@ export const checkout = async (req, res) => {
   const options = {
     amount: Number(req.body.amount * 100),
     currency: "INR",
-};
+  };
 
   const order = await instance.orders.create(options);
   res.status(200).json({
