0ca1ea73f323843ab816ebea8c91481fa9ed6b3b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-07-24 11:36:36 +0530

    Utilities Completed

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken"
 import { createError } from "./error.js";
 
-export const verifyToken = (req,res,next) => {
+export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if(!token) return next(createError(401, "You are not authenticated!"));
+  if (!token) return next(createError(401, "You are not authenticated!"));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-    if(err) return next(createError(403, "Token is not valid!"));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
     next();
   })
